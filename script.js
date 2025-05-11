77a6bec2153f6de0e93362b1a4d8527b891f29b6
camila lima <cls@Prometheus.local>
2025-05-11 19:33:43 -0300

    finalizado o javascript

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ function verificarNumero() {
     const mensagem = document.getElementById('mensagem');
     mensagem.innerHTML = '';
 }
+if (numero === 5) {
+    mensagem.innerHTML = `<div class="cupom">🎉 Você foi premiado com um dia de folga! 🎉</div>`;
+  } else if (numero >= 1 && numero <= 10) {
+    mensagem.textContent = "Não foi dessa vez! Tente outro número.";
+  } else {
+    mensagem.textContent = "Por favor, insira um número válido de 1 a 10.";
+  }
