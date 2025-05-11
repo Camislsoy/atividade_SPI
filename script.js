92f8764d01c5d7e7cf6040b0f3c48d7b545674ae
camila lima <cls@Prometheus.local>
2025-05-11 19:38:36 -0300

    corrigindo erro

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ function verificarNumero() {
     const numero = parseInt(document.getElementById('numero').value);
     const mensagem = document.getElementById('mensagem');
     mensagem.innerHTML = '';
-}
-if (numero === 5) {
-    mensagem.innerHTML = `<div class="cupom">🎉 Você foi premiado com um dia de folga! 🎉</div>`;
-  } else if (numero >= 1 && numero <= 10) {
-    mensagem.textContent = "Não foi dessa vez! Tente outro número.";
-  } else {
-    mensagem.textContent = "Por favor, insira um número válido de 1 a 10.";
+  
+    if (numero === 5) {
+      mensagem.innerHTML = `<div class="cupom">🎉 Você foi premiado com um dia de folga! 🎉</div>`;
+    } else if (numero >= 1 && numero <= 10) {
+      mensagem.textContent = "Não foi dessa vez! Tente outro número.";
+    } else {
+      mensagem.textContent = "Por favor, insira um número válido de 1 a 10.";
+    }
   }
+  
