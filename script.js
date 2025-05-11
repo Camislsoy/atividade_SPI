4e2f0c3c7267dceba4d2fcaf1d1a466ed10597ae
camila lima <cls@Prometheus.local>
2025-05-11 19:31:26 -0300

    codigo para estilo

---
+++ script.js
@@ -0,0 +1,5 @@
+function verificarNumero() {
+    const numero = parseInt(document.getElementById('numero').value);
+    const mensagem = document.getElementById('mensagem');
+    mensagem.innerHTML = '';
+}
